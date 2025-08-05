7062c19433977f33a1887c2bf65657dedef6270c
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2025-08-05 14:24:00 +0200

    add formattingNeeded logic

--- lib/src/main/java/pl/otros/logview/formatter/record/JavaRecordMessageFormatter.java
+++ lib/src/main/java/pl/otros/logview/formatter/record/JavaRecordMessageFormatter.java
@@ -5,7 +5,10 @@ import pl.otros.logview.api.pluginable.MessageFormatter;
 public class JavaRecordMessageFormatter implements MessageFormatter {
     @Override
     public boolean formattingNeeded(String message) {
-        return false;
+        return message != null
+                && message.indexOf('[') >= 0
+                && message.indexOf('=') >= 0
+                && !message.contains("\n");
     }
 
     @Override
