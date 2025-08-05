f24a11550e72210c08daf42865c0eb22c09e1934
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2025-08-05 15:51:27 +0200

    add Parser class

---
+++ lib/src/main/java/pl/otros/logview/formatter/record/parser/Parser.java
@@ -0,0 +1,14 @@
+package pl.otros.logview.formatter.record.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    private Parser() {}
+
+    public static List<String> splitTopLevel(String s) {
+        // TODO implement
+        return List.of();
+    }
+}
