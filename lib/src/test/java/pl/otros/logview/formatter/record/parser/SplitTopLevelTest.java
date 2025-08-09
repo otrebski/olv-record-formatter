8575fbc4e540a689913a56f22c17f7837828a0cb
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2025-08-09 11:24:31 +0200

    refactor

--- lib/src/test/java/pl/otros/logview/formatter/record/parser/SplitTopLevelTest.java
+++ lib/src/test/java/pl/otros/logview/formatter/record/parser/SplitTopLevelTest.java
@@ -8,12 +8,18 @@ public class SplitTopLevelTest {
 
     @Test
     public void shouldSplitOnTopLevelCommas() {
+        //given
         String input = "a=1,b=Record[x=2,y=3],c=4";
+
         List<String> expected = List.of(
                 "a=1",
                 "b=Record[x=2,y=3]",
                 "c=4"
         );
-        assertEquals(Parser.splitTopLevel(input), expected);
+        //when
+        List<String> actual = Parser.splitTopLevel(input);
+
+        //then
+        assertEquals(actual, expected);
     }
 }
